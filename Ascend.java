4914d6fbcc9270389dd29cfc551e2740ca857fff
adi yoeli <adi.yoeli@post.runi.ac.il>
2025-11-06 15:43:54 +0200

    Completed assignment

--- Ascend.java
+++ Ascend.java
@@ -1,7 +1,16 @@
-// Generates three integer random numbers in a given range,
-// and prints them in increasing order.
+
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
+		double limNum = Double.parseDouble(args[0]);
+		int a = (int)(Math.random() * limNum);
+		int b = (int)(Math.random() * limNum);
+		int c = (int)(Math.random() * limNum);
+		int min = Math.min (Math.min(a, b), c);
+		int	max = Math.max (Math.max(a,b), c);
+		int middle = (a + b + c) - min - max;
+
+		System.out.println(a + " " + b + " " + c + " ");
+		System.out.println(min + " " + middle + " " + max + " ");
+
 	}
 }
--- FVCalc.java
+++ FVCalc.java
@@ -2,5 +2,15 @@
 public class FVCalc {
 	public static void main(String[] args){
 		// Replace this comment with your code
+
+		int currentValue = Integer.parseInt(args[0]);
+		double rate = Double.parseDouble(args[1]);
+		double rateCal = rate/100 + 1;
+		int years = Integer.parseInt(args[2]);
+		double futureValue = currentValue * Math.pow(rateCal, years);
+
+		System.out.println("After " + years + " years, $" + currentValue +
+		" saved at " + rate + "% will yield $" + (int) futureValue);
+		
 	}
 }
